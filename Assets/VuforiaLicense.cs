6c318d0ae9e82106760c3a8018379baf835c2837
EmreKomsal <komsal16@itu.edu.tr>
2023-11-11 13:39:29 +0300

    Friction Removed

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVRDTm9zYi8vLy8vQUFBQm1ield1MlF3Tmsvbm9oc0l1dWpGaE9xQVVzeXlwMkdYUlNsSDQyS3RBL3RQY2NpMGJzMDJyS0d3akdBMGt2dzU0Y1VsaVJ2SmFtUERWTlVsaU1ndEhMSUFPQnpNYlZ3WUM1dEtzWG5JbkZZdE4zKzBXeTZjU0RHMmhFLy9aM2FxQW5Ld2V4aWhhbW5qRFNGWkVCTkpDTEx4bGNzQnRuRitUelhuQjhwYlY3ZHEzL0M3NzRFNnZ5VS9ZTGdQUDBodDY2cm1nakdicndlYnVadGl5VDVsMDJGbWRieTFTbmgrNHRzV3czaG1vNXoxOXVPbkFVK1NDdmxnNSsvb1ZvSm5ZOSs2WnllM1BibWpuL3NlMUhiRnBTYzdUUDNvQTJlNXFOZUV2akZqUGFxTTVNZGJTN0dJZHNOYVJPOW96REtDQi9mTXF6eEFVVFFqYkoyei84aDgvUzlNU3ROSTJIcTBqbndXVXBEQU5MOTU=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
